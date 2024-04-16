014334cde38d0d54851b760b74e74bca088fa23d
abdocrowinho <ahlyua2@gmail.com>
2024-04-16 13:08:29 +0200

    sliding transion text

--- lib/features/splach_scrren/presentation/view/widgets/splash_screen_body.dart
+++ lib/features/splach_scrren/presentation/view/widgets/splash_screen_body.dart
@@ -1,10 +1,26 @@
-import 'dart:ui';
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class SplachScreenBody extends StatelessWidget {
+class SplachScreenBody extends StatefulWidget {
   const SplachScreenBody({super.key});
+
+  @override
+  State<SplachScreenBody> createState() => _SplachScreenBodyState();
+}
+
+class _SplachScreenBodyState extends State<SplachScreenBody>
+    with TickerProviderStateMixin {
+  late AnimationController animationControllerForText1;
+  late Animation<Offset> slidingAnimtion;
+  late AnimationController animationControllerForText2;
+  late Animation<Offset> slidingAnimtion2;
+  @override
+  void initState() {
+    super.initState();
+    animtionForTextOne();
+    animtionForText2();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,15 +37,62 @@ class SplachScreenBody extends StatelessWidget {
             ),
           ),
         ),
-        Text(
-          'Food App',
-          style: TextStyle(
-              fontFamily: 'Alkatra',
-              color: Colors.transparent.withOpacity(.3),
-              fontSize: 40,
-              fontWeight: FontWeight.bold),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedBuilder(
+              animation: slidingAnimtion,
+              builder: (BuildContext context, Widget? child) => SlideTransition(
+                position: slidingAnimtion,
+                child: Text(
+                  'Food ',
+                  style: TextStyle(
+                      fontFamily: 'Alkatra',
+                      color: Colors.transparent.withOpacity(.3),
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            AnimatedBuilder(
+              animation: slidingAnimtion2,
+              builder: (BuildContext context, Widget? child) => SlideTransition(
+                position: slidingAnimtion2,
+                child: Text(
+                  'App',
+                  style: TextStyle(
+                      fontFamily: 'Alkatra',
+                      color: Colors.transparent.withOpacity(.3),
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          ],
         )
       ],
     );
+  }
+
+  void animtionForTextOne() {
+    animationControllerForText1 = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 750),
+    );
+    slidingAnimtion =
+        Tween<Offset>(begin: const Offset(3, 5), end: Offset(0, 0))
+            .animate(animationControllerForText1);
+    animationControllerForText1.forward();
+  }
+
+  void animtionForText2() {
+    animationControllerForText2 = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1000),
+    );
+    slidingAnimtion2 =
+        Tween<Offset>(begin: const Offset(-3, 5), end: Offset(0, 0))
+            .animate(animationControllerForText2);
+    animationControllerForText2.forward();
   }
 }
