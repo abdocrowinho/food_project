9a2cfa57f0b306a449ddb072ff70dce41453f892
abdocrowinho <ahlyua2@gmail.com>
2024-04-16 12:30:48 +0200

    add alkatra font & logo , text  for splash screen

--- lib/features/splach_scrren/presentation/view/splash_Screen.dart
+++ lib/features/splach_scrren/presentation/view/splash_Screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:food_project/features/splach_scrren/presentation/view/widgets/splash_screen_body.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -7,19 +6,21 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-          gradient: LinearGradient(
-        colors: [
-          Colors.white,
-          Colors.green,
-        ],
-        begin: Alignment.topCenter,
-        end: Alignment.bottomCenter,
-      )),
-      child: const Scaffold(
-        backgroundColor: Colors.transparent,
-        body: SplachScreenBody(),
+    return SafeArea(
+      child: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+          colors: [
+            Colors.white,
+            Colors.green,
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        )),
+        child: const Scaffold(
+          backgroundColor: Colors.transparent,
+          body: SplachScreenBody(),
+        ),
       ),
     );
   }
--- lib/features/splach_scrren/presentation/view/widgets/splash_screen_body.dart
+++ lib/features/splach_scrren/presentation/view/widgets/splash_screen_body.dart
@@ -1,14 +1,33 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class SplachScreenBody extends StatelessWidget {
   const SplachScreenBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       children: [
-        Center(
-          child: Text('start'),
+        const SizedBox(
+          height: 80,
+        ),
+        Container(
+          height: 300,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/logo.png'),
+            ),
+          ),
+        ),
+        Text(
+          'Food App',
+          style: TextStyle(
+              fontFamily: 'Alkatra',
+              color: Colors.transparent.withOpacity(.3),
+              fontSize: 40,
+              fontWeight: FontWeight.bold),
         )
       ],
     );
